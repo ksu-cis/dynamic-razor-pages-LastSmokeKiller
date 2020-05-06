6327882737df391976371604723f7637f7b4d514
LastSmokeKiller <54453964+LastSmokeKiller@users.noreply.github.com>
2020-05-05 22:11:35 -0500

    done

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,28 +9,32 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-       
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; } = "";
 
         public IEnumerable<Movie> Movies {get; protected set;}
 
-   
+        [BindProperty(SupportsGet = true)]
         public string[] MPAARatings {get;set;}
 
-       
+        [BindProperty(SupportsGet = true)]
         public string[] Genres {get;set;}
 
+        [BindProperty(SupportsGet = true)]
         public double? RTMin { get; set; }
-
+       
+        [BindProperty(SupportsGet =true)]
         public double? RTMax { get; set; }
         
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMin {get; set;}
 
-        
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMax {get;set;}
 
         public void OnGet(double? IMDBMax, double? IMDBMin, double? RTMax, double? RTMin)
         {
+            /*
             this.IMDBMin = IMDBMin;
             this.IMDBMax = IMDBMax;
             this.RTMin = RTMin;
@@ -43,6 +47,55 @@ namespace Movies.Pages
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
             Movies = MovieDatabase.FilterByRotten(Movies, RTMin, RTMax);
+            */
+            Movies = MovieDatabase.All;
+            if (SearchTerms != null)
+            {
+                Movies = from movie in Movies
+                         where movie.Title != null
+    && movie.Title.Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase)
+                         select movie;
+            }
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie => movie.MPAARating != null
+                && MPAARatings.Contains(movie.MPAARating));
+            }
+            if(Genres != null && Genres.Length != 0)
+            {
+                Movies = Movies.Where(movie => movie.MajorGenre != null &&
+                Genres.Contains(movie.MajorGenre));
+            }
+            if(IMDBMax != null && IMDBMin != null)
+            {
+                if(IMDBMin == null)
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating <= IMDBMax);
+                }
+                if(IMDBMax == null)
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating >= IMDBMin);
+                }
+                else
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating >= IMDBMin && movie.IMDBRating <= IMDBMax);
+                }
+            }
+            if(RTMax != null && RTMin != null)
+            {
+                if(RTMin == null)
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating <= RTMax);
+                }
+                if(RTMax == null)
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating >= RTMin);
+                }
+                else
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating >= RTMin && movie.RottenTomatoesRating <= RTMax);
+                }
+            }
         }
     }
 }
